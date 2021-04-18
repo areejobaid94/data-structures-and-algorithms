f2895b2b853c010a89d2f43bf669731c634b42bb
areejobaid94 <areej.obaid@yahoo.com>
2021-04-18 18:32:39 +0300

    done-lab-11

---
+++ Data-Structures/stacksandqueues/app/src/main/java/stacksandqueues/PseudoQueue.java
@@ -0,0 +1,43 @@
+package stacksandqueues;
+
+public class PseudoQueue<T> {
+    Stack stackQueue;
+    Stack stackHelper;
+
+    public PseudoQueue() {
+        this.stackQueue = new Stack();
+        this.stackHelper = new Stack();
+    }
+
+    public void enqueue(T value){
+        try {
+            while (!stackQueue.isEmpty()){
+                stackHelper.push(stackQueue.pop());
+            }
+            stackQueue.push(value);
+            while (!stackHelper.isEmpty()){
+                stackQueue.push(stackHelper.pop());
+            }
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "PseudoQueue{" +
+                "stackQueue=" + stackQueue +
+                ",stackHelper=" +stackHelper +
+                '}';
+    }
+    public Object dequeue(){
+        Object value = new Object();
+        try {
+            value = stackQueue.pop();
+        }catch (NullPointerException ex){
+            System.out.println(ex);
+        }
+        return value;
+    }
+}
