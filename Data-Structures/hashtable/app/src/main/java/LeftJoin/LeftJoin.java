b09736d098a0fd7f4e3894c62c0ac8651deea608
areejobaid94 <areej.obaid@yahoo.com>
2021-05-28 00:44:06 +0300

    done

--- Data-Structures/hashtable/app/src/main/java/LeftJoin/LeftJoin.java
+++ Data-Structures/hashtable/app/src/main/java/LeftJoin/LeftJoin.java
@@ -6,25 +6,18 @@ import linkedList.LinkedList;
 import linkedList.Node;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LeftJoin {
-    public static ArrayList leftJoin(Hashtable leftHashtable,Hashtable rightHashtable){
+    public static ArrayList leftJoin(HashMap leftHashtable, HashMap rightHashtable){
         ArrayList output = new ArrayList();
-        for(int i = 0 ; i < leftHashtable.getHashNodesList().length ;i++){
-            if(leftHashtable.getHashNodesList()[i] != null){
-                Node current =((LinkedList) leftHashtable.getHashNodesList()[i]).head;
-                ArrayList inOutput = new ArrayList();
-                while (current != null){
-                    HashTableNode node =  ((HashTableNode) current.val);
-                    inOutput = new ArrayList();
-                    inOutput.add(node.getKey());
-                    inOutput.add(node.getValue());
-                    inOutput.add(rightHashtable.get(node.getKey()));
-                    current = current.next;
-                }
-                output.add(inOutput);
-            }
-        }
+        leftHashtable.forEach((k, v) -> {
+            ArrayList inOutput = new ArrayList();
+            inOutput.add(k);
+            inOutput.add(v);
+            inOutput.add(rightHashtable.get(k));
+            output.add(inOutput);
+        });
         return output;
     }
 }
